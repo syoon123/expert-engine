5ccb5513d5e41d0a9d1d4d023cfc6383bc1e5cdf
Sarah Yoon <syoon2@stuy.edu>
2017-03-08 01:19:29 -0500

    idkkk

--- parser.py
+++ parser.py
@@ -38,11 +38,11 @@ def parse_file( fname, points, transform, screen, color ):
     color = [0,255,0]
     
     while (line != ""):
-        command = line
-        args = f.readline().split(" ")
+        command = line.strip("\n")
+        args = f.readline().strip("\n").split(" ")
 
         if (command == "line"):
-            add_edge(points, args[0], args[1], args[2], args[3], args[4], args[5])
+            add_edge(points, int(args[0]), int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]))
             print "line added"
 
         if (command == "display"):
@@ -56,26 +56,26 @@ def parse_file( fname, points, transform, screen, color ):
             print "reset transformation matrix"
 
         if (command == "scale"):
-            temp = make_scale(args[0], args[1], args[2])
+            temp = make_scale(int(args[0]), int(args[1]), int(args[2]))
             matrix_mult(temp, transform)
             print "scaling"
 
         if (command == "rotate"): 
             
             if (args[0] == "x"):
-                temp = make_rotX(args[1])
+                temp = make_rotX(int(args[1]))
 
             if (args[0] == "y"):
-                temp = make_rotY(args[1])
+                temp = make_rotY(int(args[1]))
 
             if (args[0] == "z"):
-                temp = make_rotZ(args[1])
+                temp = make_rotZ(int(args[1]))
 
             matrix_mult(temp, transform)
             print "rotating"
 
         if (command == "move"):
-            temp = make_translate(args[0], args[1], args[2])
+            temp = make_translate(int(args[0]), int(args[1]), int(args[2]))
             matrix_mult(temp, transform)
             print "translating"
 
